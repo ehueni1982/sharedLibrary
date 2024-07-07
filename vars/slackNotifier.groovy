4c37df7453fbf30cd2dd39f3f56e69bd6652f562
ehueni1982 <bartelemyangora@gmail.com>
2024-07-07 22:15:03 +0200

    2e commit

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 
-def call(string buildResult) {
+def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
     slackSend color: "good", message: "CONGRATULATION: Job ${env.JOB_NAME} with builnumber ${env.BUILD_NUMBER} was successful ! more info ${env.BUILD_URL}"
   }
